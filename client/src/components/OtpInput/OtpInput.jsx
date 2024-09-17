46e5cabff95bdbf32975bb16cfc6ba4413ffd85c
ndcarepvt <githubndcare@gmail.com>
2024-09-17 09:12:45 +0530

    add otp paste func

--- client/src/components/OtpInput/OtpInput.jsx
+++ client/src/components/OtpInput/OtpInput.jsx
@@ -113,7 +113,7 @@ const OtpInput = ({ length = 6, onOtpSubmit = () => {} }) => {
                 onPaste={handlePaste}
                 autoComplete="one-time-code"
                 className="otp-input w-[60px] h-[60px] m-[5px] text-[1.2rem] text-center border-2 border-solid border-black"
-                maxLength={1} // Ensure only one character per input
+                // maxLength={1} // Ensure only one character per input
               />
             ))}
             <button
