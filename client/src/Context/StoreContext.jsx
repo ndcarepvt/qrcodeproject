f6e7539f0ba8bf03ea70acc093ba239d05e28307
ndcarepvt <githubndcare@gmail.com>
2024-09-20 13:33:26 +0530

    set backend url

--- client/src/Context/StoreContext.jsx
+++ client/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 export const storeContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const URL = "http://localhost:3000";
+  const URL = "https://leadgen.ndcarenirogam.com";
   const [token, setToken] = useState("");
   const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
